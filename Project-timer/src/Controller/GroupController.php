8a69b1adb4f157696ade74d5a0b3eacd094fee9f
Rodolphe de Scorraille <rodolphe@sixtrone.com>
2020-05-29 12:30:11 +0200

    add group listing vew

---
+++ Project-timer/src/Controller/GroupController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\GroupRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Group;
+use App\Entity\User;
+use App\Form\GroupType;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Common\Collections\ArrayCollection;
+
+
+class GroupController extends AbstractController
+{
+    private $entityManager;
+    private $groupRepository;
+
+    public function __construct(GroupRepository $groupRepository,EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+        $this->groupRepository = $groupRepository;
+    }
+
+    /**
+     * @Route("/group", name="group")
+     */
+    public function index()
+    {
+        $groupList = $this->groupRepository->findAll();
+        return $this->render('group/index.html.twig', [
+            'controller_name' => 'Group Controller',
+            'group_list' => $groupList
+        ]);
+    }
+
+    /**
+     * @Route("/group_create", name="group-create")
+     */
+    public function newAction(
+        Request $request,
+        EntityManagerInterface $entityManager)
+    {
+        $group = new Group();
+        $form = $this->createForm(GroupType::class, $group);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $curentUser = $this->getUser(); 
+            $curentUserId = $curentUser->getId();
+            $group->setGroupAdmin($curentUserId);
+            $group->addUser($curentUser);
+
+            $entityManager->persist($group);
+            $entityManager->flush();
+            $this->addFlash('success', "The user has been created");
+
+            return $this->redirectToRoute('group');
+
+        }
+
+        return $this->render('group/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
