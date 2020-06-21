072543e359194c17b4ef33f1caf0a946e2a57a90
Abdoulaye224 <traoreabdoulaye097@gmail.com>
2020-06-21 16:36:21 +0200

    fix: script modified

--- Project-timer/scripts/start.sh
+++ Project-timer/scripts/start.sh
@@ -4,7 +4,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $SCRIPTPATH
 
 cd ../
-docker-compose up -d
+docker-compose up --build -d
 sleep 3
 docker-compose exec web php bin/console doctrine:migration:migrate
 cd -
